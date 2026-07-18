684305ae33b0b3f9c80b3e12b609a18c950e5679
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-18 15:48:24 -0700

    Add default configuration and localization for tablet accessory

--- common/src/main/java/dev/matthiesen/cobblemon_tablet/common/accessories/TabletItemAccessory.java
+++ common/src/main/java/dev/matthiesen/cobblemon_tablet/common/accessories/TabletItemAccessory.java
@@ -2,6 +2,10 @@ package dev.matthiesen.cobblemon_tablet.common.accessories;
 
 import dev.matthiesen.cobblemon_tablet.common.item.TabletItem;
 import io.wispforest.accessories.api.Accessory;
+import net.minecraft.world.item.ItemStack;
 
 public record TabletItemAccessory<T extends TabletItem>(T item) implements Accessory {
+    public boolean canEquipFromUse(ItemStack stack) {
+        return false;
+    }
 }
